65098cd3ef71ac4f6502d2910e79c1792bb05c52
Gustavo Maia <gurumaia@gmail.com>
2019-04-15 14:41:45 -0300

    Add BASE_DIR

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
+BASE_DIR=$(dirname -- $0)
+
 read -p 'Enter LOCAL_PATH: ' LOCAL_PATH
 read -p 'Enter REMOTE_PATH: ' REMOTE_PATH
 
 export LOCAL_PATH REMOTE_PATH
 
-envsubst < pre-commit.template.sh > .git/hooks/pre-commit
+envsubst < ${BASE_DIR}/pre-commit.template.sh > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
-envsubst < post-commit.template.sh > .git/hooks/post-commit
+envsubst < ${BASE_DIR}/post-commit.template.sh > .git/hooks/post-commit
 chmod +x .git/hooks/post-commit
