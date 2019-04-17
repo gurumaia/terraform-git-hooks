bd6750b37c019f1cab754203001db80e3fa6a515
Gustavo Maia <gurumaia@gmail.com>
2019-04-17 17:55:22 -0300

    Change argument syntax and add gitignore

--- setup.sh
+++ setup.sh
@@ -1,11 +1,23 @@
 BASE_DIR=$(dirname -- $0)
 
-read -p 'Enter LOCAL_PATH: ' LOCAL_PATH
-read -p 'Enter REMOTE_PATH: ' REMOTE_PATH
+while getopts ":l:r:" opt; do
+  case $opt in
+    l) LOCAL_PATH="$OPTARG"
+    ;;
+    r) REMOTE_PATH="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
 
 export LOCAL_PATH REMOTE_PATH
+
+echo "LOCAL_PATH: ${LOCAL_PATH}"
+echo "REMOTE_PATH: ${REMOTE_PATH}"
 
 envsubst < ${BASE_DIR}/pre-commit.template.sh > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 envsubst < ${BASE_DIR}/post-commit.template.sh > .git/hooks/post-commit
 chmod +x .git/hooks/post-commit
+cp ${BASE_DIR}/gitignore .gitignore
