b75cf9e759c352e06b80442181a9627201e24d75
Gustavo Maia <gurumaia@gmail.com>
2019-04-17 11:26:13 -0300

    Improve find exclude pattern

--- post-commit.template.sh
+++ post-commit.template.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #
-find . -path ./.terraform -prune -o -name '*.tf' -exec sed -i "s,${REMOTE_PATH},${LOCAL_PATH}," {} \;
+find . -path './.terraform' -prune -o -path '*/.terraform' -prune -o -name '*.tf' -exec sed -i "s,${REMOTE_PATH},${LOCAL_PATH}," {} \;
